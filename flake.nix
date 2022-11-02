0539cf4fb9feee865ed2e123899ccb323ee5a2b6
David <david@zarel.net>
2022-11-02 13:14:15 +0100

    Add support for direnv

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
     devShell.x86_64-linux = mkShell {
+      hardeningDisable = [ "fortify" ];
       buildInputs = [
         go_1_18
         gotools # provides godoc
