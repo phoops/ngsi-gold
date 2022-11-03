417e4e2decfb20266084d5f1f18b72905e468fbe
David <david@zarel.net>
2022-11-03 20:17:32 +0100

    Wrap Client errors in specific type

--- client/errors.go
+++ client/errors.go
@@ -4,5 +4,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-var ErrMissingURL = errors.New("invalid client configuration: missing URL")
-var ErrNegativeTimeout = errors.New("invalid client configuration: negative HTTP timeout")
+type ErrInvalidClientConfig error
+
+var ErrMissingURL ErrInvalidClientConfig = errors.New("invalid client configuration: missing URL")
+var ErrNegativeTimeout ErrInvalidClientConfig = errors.New("invalid client configuration: negative HTTP timeout")
